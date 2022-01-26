55700a7953444c570b22b01881411424ed9ba11e
milenamanko <milenamankowska@gmail.com>
2022-01-26 16:42:39 +0100

    added junit tests

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,54 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelperTest {
 
-	
+    @Test
+    public void test() {
+        Assert.assertEquals("a", "a");
+    }
+
+    @Test
+    public void getCountTest() {
+        List<String> empNames = Arrays.asList("Carol", "Peter", "Julia");
+        Assert.assertEquals(3, Helper.getCount(empNames));
+    }
+
+    @Test
+    public void getStatsTest() {
+        List<Integer> expYears = Arrays.asList(25, 4, 1,9, 16);
+        Assert.assertEquals(25, Helper.getStats(expYears).getMax());
+    }
+
+    @Test
+    public void getStringsOfLength3() {
+        List<String> empNames = Arrays.asList("Carol", "Peter", "Joe", "Julia");
+        Assert.assertEquals(1, Helper.getStringsOfLength3(empNames));
+    }
+
+    @Test
+    public void getSquareList() {
+        List<Integer> expYears = Arrays.asList(25, 4, 1,9, 16);
+        Assert.assertEquals(Arrays.asList(25*25, 4*4, 1*1, 9*9, 16*16), Helper.getSquareList(expYears));
+    }
+
+    @Test
+    public void getMergedListTest() {
+        List<String> empName = Arrays.asList("Carol", "Peter", "Julia");
+        Assert.assertEquals("Carol, Peter, Julia", Helper.getMergedList(empName));
+    }
+
+    @Test
+    public void getFilteredListTest() {
+        List<String> empName = Arrays.asList("Carol", "Peter", "Julia", "");
+        Assert.assertEquals(Arrays.asList("Carol", "Peter", "Julia"), Helper.getFilteredList(empName));
+    }
 }
